c49676dbc5b6db29ef059f422513ce149a988400
Orangel Hernandez <orangel.hernandez1999@gmail.com>
2024-11-27 20:47:09 +0000

    añadidos nuevos miembros para la familia y metodos para crear id, añadir miembros, eleiminar miembros, obtener a la familia y a cada miembro de ella

--- src/app.py
+++ src/app.py
@@ -15,6 +15,32 @@ CORS(app)
 # create the jackson family object
 jackson_family = FamilyStructure("Jackson")
 
+Sebastian = {
+    "first_name": "Sebastian",
+    "last_name": jackson_family.last_name,
+    "age": 25,
+    "lucky_numbers": [7, 27, 13]
+}
+
+Aitor = {
+    "first_name": "Aitor",
+    "last_name": jackson_family.last_name,
+    "age": 30,
+    "lucky_numbers": [11, 21, 31]
+}
+
+Albert = {
+    "first_name": "Albert",
+    "last_name": jackson_family.last_name,
+    "age": 55,
+    "lucky_numbers": [7, 35, 29]
+}
+
+jackson_family.add_member(Sebastian)
+jackson_family.add_member(Aitor)
+jackson_family.add_member(Albert)
+
+
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
@@ -31,13 +57,43 @@ def handle_hello():
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
     response_body = {
-        "hello": "world",
         "family": members
     }
-
-
     return jsonify(response_body), 200
 
+@app.route('/member/<int:id>', methods=['DELETE'])
+
+def delete_member(id):
+    funciona = jackson_family.delete_member(id)
+    if funciona:
+        return jsonify({"message": "Member deleted successfully"}), 200
+    return jsonify({"error": "Member not found"}), 404
+
+@app.route('/member/<int:id>', methods=['GET'])
+def get_member(id):
+    miembro = jackson_family.get_member(id)
+    if miembro:
+        return jsonify(miembro), 200
+    return jsonify({"error": "Member not found"}), 404
+
+@app.route('/members', methods=['POST'])
+def add_member():
+    data = request.json
+
+    jackson_family.add_member(data)
+    return jsonify("miembro añadido"), 200
+
+
+
+print("Miembros actuales:", jackson_family.get_all_members())
+
+
+print("Obtener miembro por ID:", jackson_family.get_member(Albert["id"]))
+
+print("Eliminar miembro:", jackson_family.delete_member(Albert["id"]))
+
+
+print("Miembros restantes:", jackson_family.get_all_members())
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
